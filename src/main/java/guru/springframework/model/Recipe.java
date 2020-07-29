4a37f741923929967578db519da109c80bf18725
jcascioli <joeacascioli@gmail.com>
2020-07-29 13:55:05 -0400

    added convenience method for adding ingredients

--- src/main/java/guru/springframework/model/Recipe.java
+++ src/main/java/guru/springframework/model/Recipe.java
@@ -39,6 +39,12 @@ public class Recipe {
         inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        ingredients.add(ingredient);
+        return this;
+    }
+
     public Long getId() {
         return id;
     }
